626b748b85fd738d573479d134b283f4144a38ad
zain786-cybersecurity <zainshabaan@outlook.com>
2025-12-05 16:23:12 +0000

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello from my own DevOps CI/CD Project so excited to share it with you!"
-
-if __name__ == "__main__":
-    app.run()
